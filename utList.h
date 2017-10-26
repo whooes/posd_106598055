bd12b555046d0ac0ba556c4543b3f748d45a9a98
whooes <whooeswhooes@gmail.com>
2017-10-26 14:20:23 +0800

    fix

--- utList.h
+++ utList.h
@@ -90,7 +90,13 @@ TEST(List, matchToStructShouldFail) {
 // ?- Y = [496, X, terence_tao].
 // Y = [496, X, terence_tao].
 TEST(List, matchToVarShouldSucceed) {
-  
+  Variable Y("Y");
+  Number num(496);
+  Variable X("X");
+  Atom terence_tao("terence_tao");
+  std::vector<Term *> v ={&num ,&X ,&terence_tao};
+  List l(v);
+  EXPECT_TRUE(Y.match(l));
 
   //std::cout << Y.value() << std::endl;
 }
